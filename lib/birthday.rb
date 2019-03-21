741655f7035585328919768e9884d43b69e45042
Matthew Bartl <mattbartl@gmail.com>
2019-03-21 00:30:54 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,7 +11,4 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday
-  birthday_kids.each do |kids_name, age|
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
 end
